9eed35ef2bbfc0555ffca8b110da78bf0ff5e2f7
Your Name <you@example.com>
2025-06-27 19:06:35 +0200

    crear dto create del producto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const versionApi = config.get<string>('API_VERSION') || '1'
+  app.setGlobalPrefix(`api/v${versionApi}`)
+  // Para funciones class-validator de dto
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,43 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsDecimal,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @MinLength(1)
+  slug: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsDecimal()
+  @IsOptional()
+  price?: number;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
